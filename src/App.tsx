82f9b7a2db1cdb54da369f78b2f830f1e81e9643
linhdh <linhdh@inmobivn.com>
2024-05-06 16:33:58 +0700

    config code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
+import { useState } from 'react';
 // import './App.css'
-import './index.css'
+import './index.css';
+import MyBonusToday from './components/MyBonusToday';
+import TopPlayers from './components/TopPlayers';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="first">
       <h1>ZODIAC</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <MyBonusToday/>
+      <TopPlayers/>
+
+    </div>
+
     </>
   )
 }
---
+++ src/components/MyBonusToday.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MyBonusToday() {
+    return (
+        <>  
+            <h4>Thưởng hôm nay</h4>
+            <h4>Số lần dự đoán hôm nay:</h4>
+            <h4>Tổng của tôi:</h4>
+            <h4>Nạp ngay</h4>
+        </>
+    );
+}
+
+export default MyBonusToday;
