65b2b212ef81d6d4bf55ff4e5fe33bada985ec0a
linhdh <linhdh@inmobivn.com>
2024-05-16 14:29:05 +0700

    add "/" in css

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,50 +1,34 @@
-import React from 'react';
 import backgroundSelected from '../assets/b.svg';
 import dragon from '../assets/dragon.svg';
 import Icoin from '../assets/icoin.svg';
-import BgContent from '../assets/background-betting-content.svg';
 
-interface PopupProps {
-  show: boolean;
-  onClose: () => void;
-}
+import BgContent from '../assets/bg-betting-content.png';
 
-const Popup: React.FC<PopupProps> = ({ show, onClose }) => {
-  // Return null if show is false
+
+const Popup = ({ show , onClose}) => {
   if (!show) {
     return null;
   }
 
   return (
-    <>
-      <div className="popup-overlay">
+    <div className="popup-overlay">
         <div className="popup">
-          {/* Image components */}
-          <img src={BgContent} alt="Background content" className='betting__bg-content' />
-          <img src={backgroundSelected} alt="Selected background" className='betting__zodiac-background' />
-          <img src={dragon} alt="Zodiac card" className='betting__zodiac-card' />
-          
-          {/* Text */}
-          <p className="betting__text mt-8px">Chúc bạn nhận thưởng lớn</p>
-          
-          {/* Total iCoin */}
-          <div className="betting__total-icoin mb-30px mt-29px">
-            <img className='betting__total-icoin-img' src={Icoin} alt="Icoin" />
-            <p className='betting__total-icoin-value'>100</p>
+        <img src={BgContent} alt="card_background" className='betting--BgContent'></img>
+          <img src={backgroundSelected} alt="card_background" className='betting--zodiac-background'></img>
+          <img src={dragon} alt="card_zodiac" className='betting--zodiac-card'></img>
+          <p className="betting--text mt-8px">Chúc bạn nhận thưởng lớn</p>
+          <div className ="betting__totalIcoin mb-30px mt-29px">
+            <img className='betting__totalIcoin--img' src={Icoin} alt="" />
+            <p className='betting__totalIcoin--icoin'>100</p>
           </div>
-          
-          {/* Buttons */}
-          <button className="betting__button">+10</button>
-          <button className="betting__button betting__button--alt">+100</button>
-          <button className="betting__button betting__button--alt-2">+1000</button>
-          
-          {/* Confirm Button */}
-          <div onClick={onClose} className="betting__confirm mb-33px mt-29px">
-            <p className='betting__confirm-text'>Xác nhận</p>
+          <div className ="betting--button">+10</div>
+          <div className ="betting--button-2">+100</div>
+          <div className ="betting--button-3">+1000</div>
+          <div onClick={onClose} className ="betting__confirm mb-33px mt-29px">
+            <p className='betting__confirm--text'>Xác nhận</p>
           </div>
         </div>
-      </div>
-    </>
+    </div>
   );
 };
 
