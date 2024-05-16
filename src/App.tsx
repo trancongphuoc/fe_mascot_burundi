78e535f744950cfdc052b939b9c4d08eccc271fb
linhdh <linhdh@inmobivn.com>
2024-05-16 22:46:02 +0700

    betting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './view/Home';
+import Home from './view/Home.tsx';
 
 
 function App() {
--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,33 +1,48 @@
+import React, { useState } from 'react';
 import backgroundSelected from '../assets/b.svg';
 import dragon from '../assets/dragon.svg';
 import Icoin from '../assets/icoin.svg';
-
 import BgContent from '../assets/bg-betting-content.png';
+import BgHeader from '../assets/bg-betting-header.png';
+import BgLighter from '../assets/background-betting-lighter.svg';
 
+interface PopupProps {
+  show: boolean;
+  onClose: () => void; // Define onClose prop as a function that takes no arguments and returns void
+}
 
-const Popup = ({ show , onClose}) => {
+const Popup: React.FC<PopupProps> = ({ show, onClose }) => {
+
+  const [stake, setStake] = useState(0);
+
   if (!show) {
     return null;
   }
 
   return (
     <div className="popup-overlay">
-        <div className="popup">
+      <div className="popup">
         <img src={BgContent} alt="card_background" className='betting--BgContent'></img>
-          <img src={backgroundSelected} alt="card_background" className='betting--zodiac-background'></img>
-          <img src={dragon} alt="card_zodiac" className='betting--zodiac-card'></img>
-          <p className="betting--text mt-8px">Chúc bạn nhận thưởng lớn</p>
-          <div className ="betting__totalIcoin mb-30px mt-29px">
-            <img className='betting__totalIcoin--img' src={Icoin} alt="" />
-            <p className='betting__totalIcoin--icoin'>100</p>
-          </div>
-          <div className ="betting--button">+10</div>
-          <div className ="betting--button-2">+100</div>
-          <div className ="betting--button-3">+1000</div>
-          <div onClick={onClose} className ="betting__confirm mb-33px mt-29px">
-            <p className='betting__confirm--text'>Xác nhận</p>
-          </div>
+        <img src={backgroundSelected} alt="card_background" className='betting--zodiac-background'></img>
+        <img src={dragon} alt="card_zodiac" className='betting--zodiac-card'></img>
+
+        <img src={BgLighter} alt="betting lighter" className='betting--BgLighter'></img>
+        <img src={BgHeader} alt="betting header" className='betting--BgHeader'></img>
+
+        <p className="betting--text">Chúc bạn nhận thưởng lớn</p>
+
+        <div className ="betting__totalIcoin mb-15-5px mt-28px">
+          <img className='betting__totalIcoin--img' src={Icoin} alt="" />
+          <p className='betting__totalIcoin--icoin'>{stake}</p>
         </div>
+
+        <div onClick={()=>setStake((stake) => stake + 10)} className ="betting--button">+10</div>
+        <div onClick={()=>setStake((stake) => stake + 100)} className ="betting--button-2">+100</div>
+        <div onClick={()=>setStake((stake) => stake + 1000)} className ="betting--button-3">+1000</div>
+        <div onClick={onClose} className ="betting__confirm mb-33px mt-14-5px">
+          <p className='betting__confirm--text'>Xác nhận</p>
+        </div>
+      </div>
     </div>
   );
 };
