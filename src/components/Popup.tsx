331b4028da6084fde82442aa158e8f1b16296827
linhdh <linhdh@inmobivn.com>
2024-05-16 10:22:11 +0700

    zodiac game

---
+++ src/components/Popup.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import backgroundSelected from '../assets/b.svg';
+import dragon from '../assets/dragon.svg';
+import Icoin from '../assets/icoin.svg';
+import Bet from '../assets/background-betting.svg';
+
+
+const Popup = ({ show, onClose}) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="popup-overlay">
+        <div className="popup">
+          <img src={backgroundSelected} alt="card_background" className='betting--zodiac-background'></img>
+          <img src={dragon} alt="card_zodiac" className='betting--zodiac-card'></img>
+          <p className="betting--text mt-8px">Chúc bạn nhận thưởng lớn</p>
+          <div className ="betting__totalIcoin mb-30px mt-29px">
+            <img className='betting__totalIcoin--img' src={Icoin} alt="" />
+            <p className='betting__totalIcoin--icoin'>100</p>
+          </div>
+          <div className ="betting--button">+10</div>
+          <div className ="betting--button-2">+100</div>
+          <div className ="betting--button-3">+1000</div>
+          <div onClick={onClose} className ="betting__confirm mb-33px mt-29px">
+            <p className='betting__confirm--text'>Xác nhận</p>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Popup;
---
+++ src/view/Home.tsx
@@ -0,0 +1,145 @@
+import '../css/index.css';
+
+import tiger from '../assets/tiger.svg';
+import buffalo from '../assets/buffalo.svg';
+import chicken from '../assets/chicken.svg';
+import dragon from '../assets/dragon.svg';
+import goat from '../assets/goat.svg';
+import horse from '../assets/horse.svg';
+import pig from '../assets/pig.svg';
+import snake from '../assets/snake.svg';
+
+import Card from '../components/Card';
+import FullCard from '../components/FullCard';
+import AvatarCircle from '../components/AvatarCircle';
+import PrimaryText from '../assets/primary-text.svg';
+import Rule from '../assets/rule.svg';
+import Arrow from '../assets/arrow.svg';
+import avatar from '../assets/avatar.png';
+
+import MyBonusToday from '../components/MyBonusToday';
+import BestPlayers from '../components/BestPlayers';
+import Bet from "./assets/background-betting.svg";
+import { useState } from 'react';
+import Popup from '../components/Popup';
+import Icoin from './assets/icoin.svg';
+import backgroundSelected from './assets/b.svg';
+//  
+
+
+interface BettingCard {
+  card: string;
+  bonus: Bonus;
+  isSelected: boolean;
+  players: number;
+}
+
+type Bonus = 5 | 8 | 10 | 15 | 20; 
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  let bets: number = 124;
+  let myInfoBetReults: CardModel[] = [{card: tiger, isSelected: true, number: 7, bonus: 15, players: 7},
+                                      {card: tiger, isSelected: true, number: 7, bonus: 15, players: 7},
+                                      {card: tiger, isSelected: true, number: 7, bonus: 15, players: 7}];
+  let leftResutls: string[] = [horse, 
+                              goat,
+                              // chicken,
+                              pig];
+  let rightResults: string[] = [avatar,
+                              avatar,
+                              // avatar,
+                              avatar,
+                              avatar]
+  let numberResult: number = 29;
+  let bettingTable: BettingCard[] = [
+                                  {card: buffalo, isSelected: false, bonus: 5, players: 0},
+                                  {card: tiger, isSelected: false, bonus: 5, players: 0},
+                                  {card: dragon, isSelected: true, bonus: 5, players: 0},
+                                  {card: snake, isSelected: false, bonus: 8, players: 10},
+                                  {card: horse, isSelected: false, bonus: 8, players: 0},
+                                  {card: goat, isSelected: false, bonus: 10, players: 20},
+                                  {card: chicken, isSelected: false, bonus: 15, players: 0},
+                                  {card: pig, isSelected: false, bonus: 20, players: 0}
+                                  ];
+  let bestPlayers: BestPlayersModel[] = [
+                                      {name: "Dong Hoang Linh", avatarUrl: "https://www.ikara.co/avatar/103929910820839711115?type=LARGE&version=8", winningIcoin: 9999},
+                                      {name: "Doan Dai Hiep", avatarUrl: "https://www.ikara.co/avatar/103929910820839711115?type=LARGE&version=8", winningIcoin: 9999},
+                                      {name: "Nguyễn Hoàng Chi", avatarUrl: "https://www.ikara.co/avatar/103929910820839711115?type=LARGE&version=8", winningIcoin: 9999}
+                                    ];
+  let counter: number = 30;  
+  
+  
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handleOpenPopup = () => {
+    setIsPopupVisible(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupVisible(false);
+  };
+                                    
+                                    
+
+                                
+
+  return (
+    <div className='main'>
+
+      <section className='section-header u-margin-top-huge1'>
+          <img src={PrimaryText} alt="primary_text" className='u-margin-minus-bottom-big'></img>
+          <p className='heading-secondary'>Hôm nay {bets} Ván</p>
+          <img src={Rule} alt="card_background" className='section-header__rule'></img>
+      </section>
+
+      <div className="result mt-7-5px">
+        <div className="result__left">
+          <p className='result__left--text'>Kết quả</p>
+          {
+            leftResutls.map((result, index) => (
+              <Card key={index} card={result} className={"card--zodiac__small"} classNameBackground={'card--zodiac__background--small mr-4px'}/>
+            ))
+          }
+          <img src={Arrow} alt="card_background" className=""></img>
+        </div>
+        <div className="result__right">
+          {
+            rightResults.map((result, index) => (
+              <AvatarCircle key={index} avatarUrl={result} className={'avatar mr-5px'}/>
+            ))
+          }
+          <h2 className='result__right--number'>{numberResult}</h2>
+        </div>
+      </div>
+
+
+      <section className="section-betting mt-5px">
+        <p className="section-betting--counter">Đếm ngược {counter}</p>
+        <div className="section-betting__content">
+          {
+            bettingTable.map((bettingCard, index) => (
+              <FullCard key={index} number={index + 1} card={bettingCard.card} isSelected={bettingCard.isSelected} bonus={bettingCard.bonus} players={bettingCard.players}/>
+            ))
+          }
+        </div>
+      </section>
+
+      <MyBonusToday bonusToday={1000} goodBets={4} totalIcoin={15.000} myInfoBetReults={myInfoBetReults}/>
+      
+      <BestPlayers bestPlayers={bestPlayers}/>
+
+      <button onClick={handleOpenPopup} className="open-popup-button">Open Popup</button>
+
+      <Popup show={isPopupVisible} onClose={handleClosePopup}>
+  
+     
+
+      </Popup>
+    </div>
+  )
+}
+
+export default App
