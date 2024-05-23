e290e65cf3526b3eccabdb1d53b7492c0f80cad6
linhdh <linhdh@inmobivn.com>
2024-05-23 14:05:32 +0700

    change img to SVG

--- src/components/MyBonusToday.tsx
+++ src/components/MyBonusToday.tsx
@@ -2,6 +2,7 @@
 import Icoin from '../assets/icoin.svg';
 import ArrowWhite from '../assets/arrow-white.svg';
 import Background from '../assets/background_card_small.svg';
+import SVG from 'react-inlinesvg';
 
 
 function MyBonusToday({bonusToday, goodBets, totalIcoin, myInfoBetReults, onOpen} : MyInfoBetResultModel) {
@@ -11,12 +12,12 @@ function MyBonusToday({bonusToday, goodBets, totalIcoin, myInfoBetReults, onOpen
                 <div className="section-myInfo__header--background">&nbsp;</div>
                 <div className="header-left">
                     <p className='header-left--text'>Thưởng hôm nay:</p>
-                    <img className='header-left--img' src={Icoin} alt="icoin" />
+                    <SVG className='header-left--img' src={Icoin}/>
                     <p className='header-left--icoin'>{bonusToday}</p>
                 </div>
                 <div onClick={onOpen} className="header-right">
                     <p className='header-right--text'>Số lần đã đoán hôm nay: {goodBets}</p>
-                    <img className='header-right--arrow' src={ArrowWhite} alt="white_arrow" />
+                    <SVG className='header-right--arrow' src={ArrowWhite}/>
                 </div>
                
                 <div className="section-myInfo__cards">
@@ -25,8 +26,8 @@ function MyBonusToday({bonusToday, goodBets, totalIcoin, myInfoBetReults, onOpen
                             <div key={index} className="card__main">
                                 <p className="card__main--background-color">&nbsp;</p>
                                 <p className="card__main--header">{myInfoBetReult.number}</p>
-                                <img src={Background} alt="background_small" className="card__main--background"/>
-                                <img src={myInfoBetReult.card} alt="" className="card__main--zodiac" />
+                                <SVG src={Background} className="card__main--background"/>
+                                <SVG src={myInfoBetReult.card} className="card__main--zodiac"/>
                                 <p className='card__main--bonus'>x{myInfoBetReult.bonus}</p>
                                 <h4 className='card__main--icoin'>{myInfoBetReult.players} iCoin</h4>
 
@@ -37,12 +38,12 @@ function MyBonusToday({bonusToday, goodBets, totalIcoin, myInfoBetReults, onOpen
 
                 <div className="end-left">
                     <p className='end-left--text'>Tổng của tôi:</p>
-                    <img src={Icoin} alt="icoin" className="end-left--img"></img>
+                    <SVG src={Icoin} className="end-left--img"/>
                     <p className='end-left--icoin'>{totalIcoin}</p>
                 </div>
                 <h4 className='end-right'>
                     <p className='end-right--text'>Nạp ngay</p>
-                    <img src={ArrowWhite} alt="arrow" className="end-right--img"></img>
+                    <SVG src={ArrowWhite} className="end-right--img"/>
                 </h4>
             </div>
         </>
