ab7b2ca62f971addb00361629202da3020f03e01
linhdh <linhdh@inmobivn.com>
2024-05-29 14:18:32 +0700

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/zodiac-game",
-  server: {
-    port: 3000
-  },
-})
+export default defineConfig(({ mode }) => {
+  const base = mode === 'development' ? '/' : '/zodiac-game/';
+
+  return {
+    plugins: [react()],
+    base: base,
+    server: {
+      port: 3000, // Default port for development
+    },
+  };
+});
