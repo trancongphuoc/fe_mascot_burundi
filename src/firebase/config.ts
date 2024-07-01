24742ef2e29e743758a9defa995d81401546808f
linhdh <linhdh@inmobivn.com>
2024-07-01 09:25:50 +0700

    hide firebase key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,31 +2,33 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
+
 const firebaseConfigDEV = {
-  apiKey: "AIzaSyCXDqJJRkEb5L6e__n0Vmyk_Bn95u7hD9s",
-  authDomain: "ikara-development.firebaseapp.com",
-  databaseURL: "https://ikara-development-default-rtdb.firebaseio.com",
-  projectId: "ikara-development",
-  storageBucket: "ikara-development.appspot.com",
-  messagingSenderId: "3268331263",
-  appId: "1:3268331263:web:2e5ed0e495e4437266aeca",
-  measurementId: "G-0ECG4DD0GD"
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY_DEV}`,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_DEV}`,
+  databaseURL:'https://ikara-development-default-rtdb.firebaseio.com',
+  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID_DEV}`,
+  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_DEV}`,
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_DEV}`,
+  appId: `${import.meta.env.VITE_FIREBASE_APP_ID_DEV}`,
+  measurementId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_DEV}`,
 };
 
 const firebaseConfigPRO = {
-  apiKey: "AIzaSyBKBvFB4YEp65cs-EywaC-rcimh9lh4ga4",
-  authDomain: "ikara4m.firebaseapp.com",
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY_PRO}`,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PRO}`,
   databaseURL: "https://ikara4m.firebaseio.com",
-  projectId: "ikara4m",
-  storageBucket: "ikara4m.appspot.com",
-  messagingSenderId: "366287804041",
-  appId: "1:366287804041:web:86dd7e7c9c839c3a97d858",
-  measurementId: "G-LDQYNCQJL4"
+  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID_PRO}`,
+  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_PRO}`,
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_PRO}`,
+  appId: `${import.meta.env.VITE_FIREBASE_APP_ID_PRO}`,
+  measurementId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_PRO}`,
 };
 
-
 const finalFirebaseConfig =  window.location.hostname.includes("ikara4m") ?
-                            firebaseConfigPRO : firebaseConfigDEV ;
+                            firebaseConfigPRO : firebaseConfigDEV;
+
+console.log('check data', finalFirebaseConfig);
 
 const app = initializeApp(finalFirebaseConfig);
 export const analytics = getAnalytics(app);    
