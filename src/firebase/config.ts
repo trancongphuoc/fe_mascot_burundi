65145383a9608400d3ca16966d6dbda6815f1e68
linhdh <linhdh@inmobivn.com>
2024-07-01 15:47:53 +0700

    fix evn

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,25 +4,25 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfigDEV = {
-  apiKey: 'AIzaSyCXDqJJRkEb5L6e__n0Vmyk_Bn95u7hD9s',
-  authDomain: 'ikara-development.firebaseapp.com',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY_DEV,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_DEV,
   databaseURL:'https://ikara-development-default-rtdb.firebaseio.com',
-  projectId: 'ikara-development',
-  storageBucket: 'ikara-development.appspot.com',
-  messagingSenderId: '3268331263',
-  appId: '1:3268331263:web:2e5ed0e495e4437266aeca',
-  measurementId: 'G-0ECG4DD0GD',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_DEV,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_DEV,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_DEV,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID_DEV,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_DEV,
 };
 
 const firebaseConfigPRO = {
-  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY_PRO}`,
-  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PRO}`,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY_PRO,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PRO,
   databaseURL: "https://ikara4m.firebaseio.com",
-  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID_PRO}`,
-  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_PRO}`,
-  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_PRO}`,
-  appId: `${import.meta.env.VITE_FIREBASE_APP_ID_PRO}`,
-  measurementId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_PRO}`,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_PRO,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_PRO,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_PRO,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID_PRO,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_PRO,
 };
 
 
