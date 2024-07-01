d6d50cc2fe7949c63d95d0cacc58efdb65558723
linhdh <linhdh@inmobivn.com>
2024-07-01 15:51:41 +0700

    add env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfigDEV = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY_DEV,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_DEV,
-  databaseURL:'https://ikara-development-default-rtdb.firebaseio.com',
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL_DEV,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_DEV,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_DEV,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_DEV,
@@ -17,7 +17,7 @@ const firebaseConfigDEV = {
 const firebaseConfigPRO = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY_PRO,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PRO,
-  databaseURL: "https://ikara4m.firebaseio.com",
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL_PRO,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_PRO,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_PRO,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_PRO,
