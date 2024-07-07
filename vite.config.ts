25bb2ccf7a8149d0de6f9f1c76260f4a7d123414
linhdh <linhdh@inmobivn.com>
2024-07-07 23:28:20 +0700

    add milion js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react-swc'
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react-swc';
+  import MillionLint from "@million/lint";
 
   // https://vitejs.dev/config/
   export default defineConfig(({ mode }) => {
     return {
-      plugins: [react()],
+      plugins: [react(), MillionLint.vite()],
       base: '/zodiac-game',
       build: {
         outDir: 'dist', // Ensure this is set to your desired output directory
