aa64a2bf91439c8c4516be19e9d9b0bd417303ce
linhdh <linhdh@inmobivn.com>
2024-07-17 10:48:48 +0700

    add ErrorBoundary to check log

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './view/Home.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/zodiac-game" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/zodiac-game" element={<Home />} />
+        </Routes>
+     </BrowserRouter>
+    </ErrorBoundary>
+
   );
 }
 
