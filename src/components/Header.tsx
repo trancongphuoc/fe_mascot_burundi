47712f30f0149f0affded145ad57e731177273ca
linhdh <linhdh@inmobivn.com>
2024-07-21 20:57:48 +0700

    add rule

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import PrimaryText from '../assets/primary-text.svg';
-import SVG from 'react-inlinesvg';
-import Rule from '../assets/rule.svg';
-import { log } from '../utils/log';
-import { useContext } from 'react';
-import { GameInfoContext } from '../store/game-info_context';
+import PrimaryText from "../assets/primary-text.svg";
+import SVG from "react-inlinesvg";
+import Rule from "../assets/rule.svg";
+import { log } from "../utils/log";
+import { useContext } from "react";
+import { GameInfoContext } from "../store/game-info_context";
 
 // interface HeaderProps {
 //     gameNo?: number,
@@ -11,17 +11,27 @@ import { GameInfoContext } from '../store/game-info_context';
 // }
 
 const Header = function Header() {
-    log('<Header />');
+  log("<Header />");
 
-    const { noGame, setModal} = useContext( GameInfoContext )
+  const { noGame, setModal, stateGame } = useContext(GameInfoContext);
 
-    return (
-        <header className='section-header u-margin-top-huge1'>
-            <SVG src={PrimaryText} className='u-margin-minus-bottom-big' />
-            <p className='heading-secondary'>Hôm nay {noGame ?? 0} Ván</p>
-            <SVG src={Rule} className='section-header__rule' onClick={() =>setModal({state: "OPEN", type: "RULE"})}/>   
-        </header>
-    );
+  const handleOpenRule = () => {
+    if (stateGame !== "RESULT" && stateGame !== "END") {
+      setModal({ state: "OPEN", type: "RULE" });
+    }
+  };
+
+  return (
+    <header className="section-header u-margin-top-huge1">
+      <SVG src={PrimaryText} className="u-margin-minus-bottom-big" />
+      <p className="heading-secondary">Hôm nay {noGame ?? 0} Ván</p>
+      <SVG
+        src={Rule}
+        className="section-header__rule"
+        onClick={handleOpenRule}
+      />
+    </header>
+  );
 };
 
 export default Header;
