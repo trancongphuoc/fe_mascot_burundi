a9e4b0432b5718efe3741d3d404f40d155fdb349
linhdh <linhdh@inmobivn.com>
2024-07-21 21:02:53 +0700

    fix game history

--- src/components/shortInfoGame/ShortGameHistory.tsx
+++ src/components/shortInfoGame/ShortGameHistory.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext } from "react";
 import Card from "../Card";
-import Arrow from '../../assets/arrow.svg';
+import Arrow from "../../assets/arrow.svg";
 import { db } from "../../firebase/config";
 import { off, onValue, ref } from "firebase/database";
 import { log } from "../../utils/log";
@@ -11,60 +11,65 @@ import { GameInfoContext } from "../../store/game-info_context";
 // }
 
 interface ZodiacCardHistory extends ZodiacCardModel {
-  lastUpdate: number,
+  lastUpdate: number;
 }
 
-const ShortGameHistory = function ShortGameHistory () {
-    log('<ShortGameHistory />')
-    const [gameHistories, setGameHistories] = useState<ZodiacCardHistory[]>([])
+const ShortGameHistory = function ShortGameHistory() {
+  log("<ShortGameHistory />");
+  const [gameHistories, setGameHistories] = useState<ZodiacCardHistory[]>([]);
 
-    const { stateGame, transactionId, setModal} = useContext(GameInfoContext);
+  const { stateGame, transactionId, setModal } = useContext(GameInfoContext);
 
-    useEffect(()=> {
-      const stateRef = ref(db, '/zodiacGame/state/zodiacCardsRecent');
+  useEffect(() => {
+    const stateRef = ref(db, "/zodiacGame/state/zodiacCardsRecent");
 
-      const handleData = (snapshot: any) => {
-          const data = snapshot.val();
-          if (data) {
-              const gameHistoriesList: ZodiacCardHistory[] = [];
-              for (const gameHistoryId in data) {
-                  if (Object.hasOwnProperty.call(data, gameHistoryId)) {
-                      const gameHistoryData = data[gameHistoryId];
-                      // const idCard = Math.random() * 1000;
-                      const player: ZodiacCardHistory = {
-                          lastUpdate: gameHistoryData.lastUpdate ?? 0,
-                          id: gameHistoryData.id ?? '',
-                          imageUrl: gameHistoryData.imageUrl ?? '',
-                          name: gameHistoryData.name,
-                          multiply: gameHistoryData.multiply,
-                      };
-                      gameHistoriesList.push(player);
-                  }
-              }
-              if (stateGame === "PREPARESTART" || gameHistories.length === 0) {
-                setGameHistories([...gameHistoriesList.map(card => ({...card}))]);
-              }
+    const handleData = (snapshot: any) => {
+      const data = snapshot.val();
+      if (data) {
+        const gameHistoriesList: ZodiacCardHistory[] = [];
+        for (const gameHistoryId in data) {
+          if (Object.hasOwnProperty.call(data, gameHistoryId)) {
+            const gameHistoryData = data[gameHistoryId];
+            // const idCard = Math.random() * 1000;
+            const player: ZodiacCardHistory = {
+              lastUpdate: gameHistoryData.lastUpdate ?? 0,
+              id: gameHistoryData.id ?? "",
+              imageUrl: gameHistoryData.imageUrl ?? "",
+              name: gameHistoryData.name,
+              multiply: gameHistoryData.multiply,
+            };
+            gameHistoriesList.push(player);
           }
-      };
-      onValue(stateRef, handleData);      
-      return () => off(stateRef, 'value', handleData);
-        
+        }
+        if (stateGame === "PREPARESTART" || gameHistories.length === 0) {
+          setGameHistories([...gameHistoriesList.map((card) => ({ ...card }))]);
+        }
+      }
+    };
+    onValue(stateRef, handleData);
+    return () => off(stateRef, "value", handleData);
   }, [stateGame, transactionId]);
 
+  const handleOpenGameHistory = () => {
+    if (stateGame !== "RESULT" && stateGame !== "END") {
+      setModal({ state: "OPEN", type: "GAMEHISTORY" });
+    }
+  };
 
-    return (
-        <div className="result__left" onClick={() => setModal({ state: "OPEN", type: "GAMEHISTORY"})}>
-          <p className='result__left--text'>Kết quả</p>
-          {gameHistories.map((result) => (
-            <Card
-              key={result.lastUpdate}
-              card={result.imageUrl}
-              className="card--zodiac__small"
-              classNameBackground="card--zodiac__background--small mr-4px" />
-          ))}
-          <img src={Arrow} alt="card_background" />
-        </div>
-    );
-}
+  return (
+    <div className="result__left" onClick={handleOpenGameHistory}>
+      <p className="result__left--text">Kết quả</p>
+      {gameHistories.map((result) => (
+        <Card
+          key={result.lastUpdate}
+          card={result.imageUrl}
+          className="card--zodiac__small"
+          classNameBackground="card--zodiac__background--small mr-4px"
+        />
+      ))}
+      <img src={Arrow} alt="card_background" />
+    </div>
+  );
+};
 
 export default ShortGameHistory;
